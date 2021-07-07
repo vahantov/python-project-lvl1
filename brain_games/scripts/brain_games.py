f4b2091d56098462739b6199ed6384b7cf53131d
vahan tovmasyan <vahantov@mail.ru>
2021-07-07 15:33:50 +0300

    step 2 hexlet

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,2 +1,14 @@
+#!/usr/bin/env python
+
+# if __name__ == '__main__':
+#     print('Welcome to the Brain Games!')
+
+def greet(who):
+    print('Hello, {}!'.format(who))
+
+def main():
+    greet('Bob')
+    greet('Ann')
+
 if __name__ == '__main__':
-    print('Welcome to the Brain Games!')
+    main()
